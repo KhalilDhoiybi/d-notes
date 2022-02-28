4ceeafc3a64614b286a18ff0aa668c89bed90bb2
Khalil Dhoiybi <kkhalildoiybi@gmail.com>
2022-02-28 15:34:03 +0100

    time and date feature

--- src/components/Panel/PanelHeader.jsx
+++ src/components/Panel/PanelHeader.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './styles/panelheader.css'
 
 function PanelHeader() {
+
+    // Date setting
+    const dateOptions = { weekday: 'short', year: 'numeric', month: 'short', day:'numeric' }
+    const currentDate = new Date().toLocaleDateString('en-UK',dateOptions)
+
+    // Time setting
+    const timeOptions = {hour: '2-digit', minute: '2-digit', hour12: true }
+    const [time, seTtime] = useState()
+    setInterval(() => {
+        const currentTime = new Date().toLocaleTimeString(timeOptions)
+        seTtime(currentTime)
+    },1000)
+
+
     return(
         <div className='panelheader'>
-            <h5>header</h5>
+            <h4>Date | {currentDate} |</h4>
+            <h4>Time | {time} |</h4>    
         </div>
     )
 }

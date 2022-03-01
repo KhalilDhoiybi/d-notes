10c4325b0f509c7b42114917ef6bff0cc77a2945
Khalil Dhoiybi <kkhalildoiybi@gmail.com>
2022-03-01 19:19:57 +0100

    creating new page input in the panel section

--- src/components/Panel/PanelContent.jsx
+++ src/components/Panel/PanelContent.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 import Pageindex from './Pageindex'
+import TextField from '@mui/material/TextField'
+import { createTheme,ThemeProvider } from '@mui/material/styles';
 import './styles/panelcontent.css'
 
 function PanelContent() {
+
+    // mui color theme
+    const theme = createTheme({
+        palette: {
+          primary: {
+              main: '#483434',
+              light: '#483434',
+              contrastText: '#483434'
+          }
+        }
+      });
 
     const pages = ['page 1','page 2','page 3']
 
@@ -14,7 +27,18 @@ function PanelContent() {
             <div className='pagesnavbar'>
                 {pages.map((page) => <Pageindex pagename={page} />)}
             </div>
-            <div className='newpagemenu'></div>
+            <div className='newpagemenu'>
+                <ThemeProvider theme={theme}>
+                    <TextField
+                    label="New page"
+                    id="standard-basic-small"
+                    variant="filled"
+                    size="small"
+                    color='primary'
+                    />
+                </ThemeProvider>
+            
+            </div>
         </div>
     )
 }

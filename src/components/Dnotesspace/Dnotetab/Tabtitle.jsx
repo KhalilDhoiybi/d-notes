675d03e04c7090250df9ea67fc66661072159f8c
Khalil Dhoiybi <kkhalildoiybi@gmail.com>
2022-03-01 17:16:15 +0100

    adding close icon to the tabtitle bar

--- src/components/Dnotesspace/Dnotetab/Tabtitle.jsx
+++ src/components/Dnotesspace/Dnotetab/Tabtitle.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import CloseIcon from '@mui/icons-material/Close'
+import IconButton from '@mui/material/IconButton'
 import './styles/tabtitle.css'
 
 function Tabtitle() {
     return(
         <div className='tabtitle'>
             <h4>Untitled - Page</h4>
+            <IconButton aria-label="close" size="small">
+                <CloseIcon fontSize="inherit" />
+            </IconButton>
         </div>
     )
 }

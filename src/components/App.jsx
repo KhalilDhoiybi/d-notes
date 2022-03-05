b3aa4157e00be47ede2e1ab0030d7569f9dcdc0c
Khalil Dhoiybi <kkhalildoiybi@gmail.com>
2022-03-05 12:18:17 +0100

    adding closing page handler

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,7 @@ import Panel from './Panel/Panel'
 function App() {
 
   // data TEST PROTOTYPE
-  const pagesDatafromDB = [{_id: '1',page_title: 'test 1',page_notes: [{_id: '01',note_title: 'title-test 1',note_content: 'content_test 1'},{_id: '011',note_title: 'title-test 11',note_content: 'content_test 11'}]},{_id: '2',page_title: 'test 2',page_notes: [{_id: '02',note_title: 'title-test 2',note_content: 'content_test 2'}]},{_id: '3',page_title: 'test 3',page_notes: [{_id: '03',note_title: 'title-test 3',note_content: 'content_test 3'}]}]
+  const pagesDatafromDB = [{_id: '1',page_title: 'test 1',page_notes: [{_id: '01',note_title: 'title-test 1',note_content: 'content_test 1'},{_id: '011',note_title: 'title-test 11',note_content: 'content_test 11'},{_id: '0111',note_title: 'title-test 111',note_content: 'content_test 111'}]},{_id: '2',page_title: 'test 2',page_notes: [{_id: '02',note_title: 'title-test 2',note_content: 'content_test 2'},{_id: '022',note_title: 'title-test 22',note_content: 'content_test 22'}]},{_id: '3',page_title: 'test 3',page_notes: [{_id: '03',note_title: 'title-test 3',note_content: 'content_test 3'}]}]
   const singlePageTest = pagesDatafromDB[0]
 
   // Data fetching from the api
@@ -30,16 +30,20 @@ function App() {
   // Data/State/info object
   const info = {data: pagesData, selectedPage: selectedPage}
 
-  // Select page function
+  // Select page handler
   function selectPageHandler(index) {
     setSelectedPage(pagesDatafromDB[index])
   }
 
+  // Close page handler
+  function closePageHandler() {
+    selectPageHandler(null)
+  }
 
   return(
     <div className='container'>
       <Panel info={info} selectpage={selectPageHandler} />
-      <Dnotespace info={info} />
+      <Dnotespace info={info} closepage={closePageHandler} />
     </div>
   )
 }
--- src/components/Dnotesspace/Dnotetab/Tabtitle.jsx
+++ src/components/Dnotesspace/Dnotetab/Tabtitle.jsx
@@ -6,8 +6,8 @@ import './styles/tabtitle.css'
 function Tabtitle(props) {
     return(
         <div className='tabtitle'>
-            <h4>{props.info.selectedPage != null ? props.info.selectedPage.page_title : 'Untitled'}</h4>
-            <IconButton aria-label="close" size="small">
+            <h4>{props.info.selectedPage != null ? props.info.selectedPage.page_title : 'Notes space'}</h4>
+            <IconButton onClick={() => props.closepage()} aria-label="close" size="small">
                 <CloseIcon fontSize="inherit" />
             </IconButton>
         </div>
